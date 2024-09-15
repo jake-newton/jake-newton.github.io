057175776a7d7a814b92016ede5e6bc9805159d7
Jake Newton <jake.newt@0dark30.com>
2024-09-15 15:37:04 -0700

    Attempt script

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    // Load header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('header').innerHTML = data);
+
+    // Load footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer').innerHTML = data);
+};
