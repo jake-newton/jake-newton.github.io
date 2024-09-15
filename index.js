c9926c8800de05e0a6155cc99fe8fe753f9bd50a
Jake Newton <58743337+jake-newton@users.noreply.github.com>
2024-09-15 13:20:02 -0700

    Initial Template

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function () {
+   
+});
+
+let timerId = setTimeout(showSale, 3000);
+
+function showSale() {
+   let popup = document.getElementById("popup");
+   popup.style.display = "block";
+}
